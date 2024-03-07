ecd926ce484066997a109b501a95a1e53653274c
Augusto0210 <162612229+Augusto0210@users.noreply.github.com>
2024-03-07 13:56:08 -0300

    Add files via upload

---
+++ atv1-1.js
@@ -0,0 +1,13 @@
+//1//
+console.clear(); //
+console.log("Ola Professor, Bom Dia!!");
+var nome = "Augusto Lopes Costa da Silva";
+var idad = 16;
+var lazer = "Jogar no computador";
+var gostos = "Assistir animes, series e filmes";
+var univer = "Programa\u00E7\u00E3o";
+var univerSegund = "Qualquer universidade que envolva numeros";
+console.clear(); //
+console.log("Meu nome \u00E9 ".concat(nome, ", minha idade \u00E9 ").concat(idad, ", tenho no momento o lazer de ").concat(lazer, ","));
+console.log("e o que mais gosto \u00E9 ".concat(gostos, ". Pretendo ingressar na universidade,at\u00E9 o "));
+console.log("momento estou tendencioso a fazer ".concat(univer, ", e minha segunda op\u00E7\u00E3o \u00E9 ").concat(univerSegund, "."));
---
+++ atv1-1.ts
@@ -0,0 +1,15 @@
+//1//
+console.clear(); //
+console.log(`Ola Professor, Bom Dia!!`);
+
+    let nome: string = `Augusto Lopes Costa da Silva`;
+    let idad: number = 16;
+    let lazer: string = `Jogar no computador`;
+    let gostos: string = `Assistir animes, series e filmes`;
+    let univer: string = `Programação`;
+    let univerSegund: string = `Qualquer universidade que envolva numeros`;
+
+ console.clear(); //
+console.log(`Meu nome é ${nome}, minha idade é ${idad}, tenho no momento o lazer de ${lazer},` );
+console.log(`e o que mais gosto é ${gostos}. Pretendo ingressar na universidade,até o `);
+console.log(`momento estou tendencioso a fazer ${univer}, e minha segunda opção é ${univerSegund}.`);
