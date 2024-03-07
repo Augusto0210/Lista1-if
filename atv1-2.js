8611aaca73dbb6a7acc011a2197da1613950b0a4
Augusto0210 <162612229+Augusto0210@users.noreply.github.com>
2024-03-07 13:56:29 -0300

    Add files via upload

---
+++ atv1-2.js
@@ -0,0 +1,17 @@
+//2//
+console.clear(); //Limpar o prompt command
+var teclado = require("prompt-sync")(); // Teclado //
+var n1 = parseFloat(teclado("Digite um numero\u00E7\u00E3o: "));
+var n2 = parseFloat(teclado("Digite um numero\u00E7\u00E3o: "));
+var n3 = parseFloat(teclado("Digite um numero\u00E7\u00E3o: "));
+var n4 = parseFloat(teclado("Digite um numero\u00E7\u00E3o: "));
+var n5 = parseFloat(teclado("Digite um numero\u00E7\u00E3o: "));
+var n6 = parseFloat(teclado("Digite um numero\u00E7\u00E3o: "));
+var n7 = parseFloat(teclado("Digite um numero\u00E7\u00E3o: "));
+var n8 = parseFloat(teclado("Digite um numero\u00E7\u00E3o: "));
+var n9 = parseFloat(teclado("Digite um numero\u00E7\u00E3o: "));
+var n10 = parseFloat(teclado("Digite um numero\u00E7\u00E3o: "));
+console.clear();
+var media = 0;
+media = (n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10) / 10;
+console.log(media);
